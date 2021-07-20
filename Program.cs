c13853e3eeea7ecb0ba1bc994ccdfcced2956b72
umme <unnipa01@yahoo.com>
2021-07-20 16:54:03 -0500

    initial copy

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient;
+            int remainder;
+
+            Console.WriteLine($"Addition result is {a + b}");
+            Console.WriteLine($"Subtraction result is {a - b}");
+            Console.WriteLine($"Multiplication result is {a * b}");
+            quotient = a / b;
+            remainder = a % b;
+            if ((a == 17) && (b == 4))
+               Console.WriteLine($"{a}/{b} is {a / b}  remainder {a % b}");
+
+            
+        }
+    }
+}
